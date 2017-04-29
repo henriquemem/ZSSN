f5cd1c3dcd7c37f116623b234ee69457e67cce70
henrique <henriquelsm.ti@gmail.com>
2017-04-29 12:11:01 -0300

    Validação de reportagem

--- ZSSN/src/main/java/br/com/zg/usuario/dao/ReportagemDaoImpl.java
+++ ZSSN/src/main/java/br/com/zg/usuario/dao/ReportagemDaoImpl.java
@@ -3,10 +3,26 @@ package br.com.zg.usuario.dao;
 import javax.inject.Named;
 
 import br.com.zg.base.dao.EntidadeDaoImpl;
+import br.com.zg.base.exception.BaseException;
 import br.com.zg.usuario.model.Reportagem;
+import br.com.zg.util.Menssagens;
 
 @Named
 public class ReportagemDaoImpl extends EntidadeDaoImpl<Reportagem> implements ReportagemDao {
 
-	
+	@Override
+	protected Reportagem beforeInsert(Reportagem entity) {
+		Reportagem reportagem = searchEntity()
+				.eq("usuarioReportado", entity.getUsuarioReportado())
+				.eq("usuarioReportador", entity.getUsuarioReportador())
+				.search();
+		
+		if(reportagem != null){
+			throw new BaseException(Menssagens.getMenssagem(
+					"erro.usuarioJaReportado", 
+					reportagem.getUsuarioReportador().getNome(), 
+					reportagem.getUsuarioReportado().getNome()));
+		}
+		return super.beforeInsert(entity);
+	}
 }
